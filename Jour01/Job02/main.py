070d9af7e255b95ef551f9e9dc99830da78d438e
nolan mahieu <nolan.mahieu@laplateforme.io>
2023-04-24 13:04:25 +0200

    premier

---
+++ Jour01/Job02/main.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = 12   
+        self.nombre2 = 3
+
+operation = Operation(12, 3)
+
+print("le nombre1 est", operation.nombre1)
+print("le nombre2 est", operation.nombre2)
