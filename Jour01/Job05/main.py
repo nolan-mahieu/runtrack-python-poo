30210673affd469358ae12f210c0c8ca424a2cf6
nolan mahieu <nolan.mahieu@laplateforme.io>
2023-04-24 13:40:33 +0200

    premier

--- Jour01/Job05/main.py
+++ Jour01/Job05/main.py
@@ -1,17 +1,32 @@
 class Animal:
     def __init__(self):
-        self.age = 0 
+        self.age = 0
         self.prenom = ""
 
     def vieillir(self):
         self.age += 1
 
+    def nommer(self, nom):
+        self.prenom = nom
+
+
+# Instanciation d'un objet Animal
 animal = Animal()
 
+# Affichage de l'attribut âge
 print(f"L'âge de l'animal {animal.age} ans")
 
+# Appel de la méthode "vieillir"
 animal.vieillir()
 
+# Affichage de la phrase demandée
 print("#L'âge de l'animal après appel la fonction vieillir")
 
+# Affichage de l'attribut âge après avoir fait vieillir l'animal
 print(f"L'âge de l'animal {animal.age} ans")
+
+# Nommer l'animal
+animal.nommer("Luna")
+
+# Affichage du nom de l'animal en console
+print(f"L'animal se nomme {animal.prenom}")
