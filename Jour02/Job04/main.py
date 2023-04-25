78965b423edc59c1d21e06b03d1c9435fd1f19b1
nolan mahieu <nolan.mahieu@laplateforme.io>
2023-04-25 11:44:18 +0200

    premier

--- Jour02/Job04/main.py
+++ Jour02/Job04/main.py
@@ -3,5 +3,13 @@ class Forme:
         return 0
 
 class Rectangle:
-    def __init__(self) -> None:
-        
+    def __init__(self, largeur, hauteur):
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def aire(self):
+        return self.largeur * self.hauteur
+    
+rect = Rectangle(5, 3)
+
+print("L'aire du rectangle est :", rect.aire())
